507fd6f851c86e504bb2f63fac0f01a521c39e84
Griffin <griffinalmquist@gmail.com>
2021-08-11 17:26:17 +1200

    Publisher getters and setters

---
+++ Publisher.py
@@ -0,0 +1,25 @@
+class Publisher:
+    # Default value for name is N/A
+    def __init__(self, name="N/A"):
+        # Strip away any spaces
+        self.__name = name.strip()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name="N/A"):
+        self.__name = name.strip()
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __hash__(self):
+        pass
