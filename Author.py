1c1efd5d66d0164ba3d2f78280498d989b55bb1a
galm020 <galm020@aucklanduni.ac.nz>
2021-08-12 22:54:12 +1200

    Version of Author.py that passes the non-hidden tests, but fails some hidden tests

--- Author.py
+++ Author.py
@@ -1,29 +1,70 @@
 class Author:
-    def __init__(self, author_id, author_full_name):
+    def __init__(self, unique_id, full_name):
+        # Default values for the id and name
+        # self.__unique_id = None
+        # self.__full_name = None
+
         # Check that the id that has been entered is an integer
-        if isinstance(author_id, int):
+        if isinstance(unique_id, int):
             # Ensure that the entered author id is a positive integer
-            if author_id < 0:
+            if unique_id < 0:
                 raise ValueError
             else:
-                self.__author_id = author_id
+                self.__unique_id = unique_id
 
         else:
             raise ValueError
 
         # Now we have to check that the entered name is a string
-        if isinstance(author_full_name, str):
-            self.__author_full_name = author_full_name
+        if isinstance(full_name, str):
+            if len(full_name.strip()) > 0:
+                self.__full_name = full_name
+            else:
+                raise ValueError
+        else:
+            raise ValueError
+
+        self.__coAuthor = None
+
+    @property
+    def full_name(self):
+        return self.__full_name
+
+    @full_name.setter
+    def full_name(self, value):
+        if isinstance(value, str):
+            self.__full_name = value
         else:
             raise ValueError
 
     @property
-    def author_full_name(self):
-        return self.__author_full_name
+    def unique_id(self):
+        return self.__unique_id
 
-    @author_full_name.setter
-    def author_full_name(self, value):
-        if isinstance(value, str):
-            self.__author_full_name = value
-        else:
-            raise ValueError
+    @unique_id.setter
+    def unique_id(self, value):
+        raise ValueError
+
+    def __repr__(self):
+        return f"<Author {self.__full_name}, author id = {self.__unique_id}>"
+
+    def __eq__(self, other):
+        if isinstance(other, Author):
+            return self.__unique_id == other.unique_id
+
+    def __lt__(self, other):
+        if isinstance(other, Author):
+            return self.__unique_id < other.unique_id
+
+    def __hash__(self):
+        return hash(self.__unique_id)
+
+    def add_coauthor(self, coauthor):
+        if isinstance(coauthor, Author):
+            self.__coAuthor = coauthor
+
+    def check_if_this_author_coauthored_with(self, author):
+        if isinstance(author, Author):
+            if self.__coAuthor is not None:
+                return author == self.__coAuthor
+            else: raise ValueError
--- test_author.py
+++ test_author.py
@@ -8,12 +8,22 @@ def default_author():
     return Author(123, "default")
 
 
-def test_invalid_author_id():
+@pytest.fixture
+def obama():
+    return Author(3675, "Barack Obama")
+
+
+# @pytest.fixture
+# def authors_list1():
+#     return Author[Author(5, "Bob"), Author(6, "Bob"), Author(1, "Jan"), Author(112, "Zane")]
+
+
+def test_invalid_unique_id():
     with pytest.raises(ValueError):
         Author(-10, "Bob")
 
 
-def test_invalid_author_idtype():
+def test_invalid_unique_idtype():
     with pytest.raises(ValueError):
         Author("Bob", "default")
 
@@ -25,11 +35,51 @@ def test_invalid_author_name():
 
 # Test setters and getters
 def test_fullname_getter(default_author):
-    assert default_author.author_full_name == "default"
+    assert default_author.full_name == "default"
 
 
 def test_fullname_setter(default_author):
     with pytest.raises(ValueError):
-        default_author.author_full_name = 10
+        default_author.full_name = 10
 
 
+# Test the setters and getters of unique id
+def test_unique_id_getter(default_author):
+    assert default_author.unique_id == 123
+
+
+def test_unique_id_setter(default_author):
+    with pytest.raises(ValueError):
+        default_author.unique_id = 10
+
+    assert default_author.unique_id == 123
+
+
+def test_repr(default_author, obama):
+    assert default_author.__repr__() == "<Author default, author id = 123>"
+    assert obama.__repr__() == "<Author Barack Obama, author id = 3675>"
+
+
+def test_equality(default_author, obama):
+    assert default_author != obama
+    assert obama == obama
+
+
+def test_lt(default_author, obama):
+    assert default_author < obama
+    assert obama > default_author
+
+    # for i in range(0, len(authors_list1) - 1):
+    #     assert authors_list1[i] < authors_list1[i + 1]
+
+
+def test_hash():
+    pass
+
+
+def test_add_coauthor():
+    pass
+
+
+def check_if_this_author_coauthored_with():
+    pass
