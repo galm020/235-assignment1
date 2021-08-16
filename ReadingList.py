b747246254b6f88839d42a2d60062450952d2c1a
Griffin <griffinalmquist@gmail.com>
2021-08-16 22:14:18 +1200

    Initial version of ReadingList.py

---
+++ ReadingList.py
@@ -0,0 +1,50 @@
+from Book import Book
+
+
+class ReadingList:
+    def __init__(self):
+        # read list of type book
+        self.__reading_list = []
+        self.__n = 0
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            if book not in self.__reading_list:
+                self.__reading_list.append(book)
+
+    def remove_book(self, book):
+        if isinstance(book, Book):
+            if book in self.__reading_list:
+                self.__reading_list.remove(book)
+
+    def select_book_to_read(self, index):
+        if isinstance(index, int):
+            if 0 <= index < len(self.__reading_list):
+                return self.__reading_list[index]
+            else:
+                return None
+
+    def size(self):
+        return len(self.__reading_list)
+
+    def first_book_in_list(self):
+        if len(self.__reading_list) == 0:
+            return None
+        else:
+            return self.__reading_list[0]
+
+    def __iter__(self):
+        self.__n = 0
+
+    def __next__(self):
+        if self.__n < len(self.__reading_list):
+            return self.__reading_list[self.__n]
+        else:
+            raise StopIteration
+
+readlist = ReadingList()
+print(f"Size of reading list: {readlist.size()}")
+readlist.add_book(Book(635, "Hitchhiker's Guide to the Galaxy"))
+readlist.add_book(Book(273, "Harry Potter"))
+readlist.add_book(Book(108, "Lord of the Rings"))
+print(readlist.first_book_in_list())
